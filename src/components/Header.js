73db4974e520b46c0f96136a25b7171610273e21
giannisgiann <jg@think-plus.gr>
2021-06-29 16:10:52 +0300

    build fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,13 @@ export default class Header extends React.Component {
   
     constructor(props) {
       super(props);
-  
+      const windowGlobal = typeof window !== 'undefined' && window;
+
       this.state = {
-        prevScrollpos: window.pageYOffset,
+        prevScrollpos: windowGlobal.pageYOffset,
         visible: true
       };
+      
     }
   
     // Adds an event listener when the component is mount.
@@ -21,9 +23,9 @@ export default class Header extends React.Component {
     }
   
     // Remove the event listener when the component is unmount.
-    componentWillUnmount() {
-      window.removeEventListener("scroll", this.handleScroll);
-    }
+    // componentWillUnmount() {
+    //   window.removeEventListener("scroll", this.handleScroll);
+    // }
   
     // Hide or show the menu.
     handleScroll = () => {
